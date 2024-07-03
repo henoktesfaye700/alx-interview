4467ada8704be6212dfd3b7ac8bbf1723ab89e9b
henoktesfaye700 <henoktesfaye700@gmail.com>
2024-07-03 14:57:12 +0300

    Final

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ N queens """
+
+
 import sys
 
 
@@ -19,26 +21,58 @@ n = int(sys.argv[1])
 
 
 def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
+    """Find all possible positions for N queens on an NxN board.
+
+    The positions are represented by a list of indices in the
+    range [0, N) where the index represents the row and the value
+    at that index represents the column.
+
+    The positions are found by using a recursive backtracking
+    algorithm. The positions are yielded from the recursive function
+    as they are found.
+
+    Parameters:
+    n (int): The size of the board. Must be at least 4.
+    i (int): The current row. Defaults to 0.
+    a (list): The list of column indices for the queens in the
+        rows above the current row. Defaults to an empty list.
+    b (list): The list of column indices plus row indices for the
+        queens in the rows above the current row. Defaults to an
+        empty list.
+    c (list): The list of column indices minus row indices for the
+        queens in the rows above the current row. Defaults to an
+        empty list.
+
+    Yields:
+    list: The list of column indices for the N queens.
+    """
     if i < n:
         for j in range(n):
             if j not in a and i + j not in b and i - j not in c:
                 yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
         yield a
+def solve(n: int) -> None:
+    """Print all possible positions for N queens on an NxN board.
 
+    The positions are represented by a list of indices in the
+    range [0, N) where the index represents the row and the value
+    at that index represents the column.
 
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
+    Parameters:
+    n (int): The size of the board. Must be at least 4.
+    """
+    k = []  # list to store the positions of the queens
+    i = 0  # current row
+    # iterate over all possible positions for the queens
     for solution in queens(n, 0):
         for s in solution:
+            # add the current position to the list
             k.append([i, s])
+            # increment the current row
             i += 1
+        # print the positions of the queens
         print(k)
         k = []
         i = 0
-
-
 solve(n)
